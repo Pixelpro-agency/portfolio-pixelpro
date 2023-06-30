526e30a3edbae29f3cd2a62e3e1594776e7a5278
brunosvara <brunosvara032@gmail.com>
2023-06-30 20:43:08 +0200

    aggiornamento foto

--- docs/app.js
+++ docs/app.js
@@ -91,39 +91,3 @@ toggleBtn.addEventListener('click', () => {
     toggleBtn.classList.toggle('active');
     linksContainer.classList.toggle('active');
 })
-
-// Aggiungi l'evento di invio del modulo
-document.getElementById('contact').addEventListener('submit', function (event) {
-    event.preventDefault(); // Interrompi l'invio del modulo predefinito
-  
-    // Ottieni i valori dei campi del modulo
-    var nome = document.getElementsByName('name')[0].value;
-      var email = document.getElementsByName('email')[0].value;
-      var telefono = document.getElementsByName('number')[0].value;
-      var giornoOra = document.getElementsByName('richiamare')[0].value;
-      var messaggio = document.getElementsByName('msg')[0].value;
-  
-    // Prepara i dati per l'invio
-    var dati = {
-      service_id: 'service_urqzcug',
-      template_id: 'template_9hbs0me',
-      user_id: 'user_T0eewF6hqySzYujwQ', // Chiave pubblica API
-      template_params: {
-        nome: nome,
-        email: email,
-        telefono: telefono,
-        giornoOra: giornoOra,
-        messaggio: messaggio
-      }
-    };
-  
-    // Invia l'email tramite EmailJS
-    emailjs.send('default_service', 'template_9hbs0me', dati)
-      .then(function (response) {
-        console.log('Email inviata con successo!', response);
-        // Aggiungi qui la logica per mostrare un messaggio di conferma o reindirizzare l'utente a una pagina di successo
-      }, function (error) {
-        console.log('Errore nell\'invio dell\'email:', error);
-        // Aggiungi qui la logica per mostrare un messaggio di errore all'utente
-      });
-  });
