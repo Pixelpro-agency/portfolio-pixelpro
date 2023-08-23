9c3b2f313661b19945e880d80e6caa03a7455bfc
brunosvara <brunosvara032@gmail.com>
2023-08-23 17:04:56 +0200

    aggiornamento 23 agosto

--- docs/script.js
+++ docs/script.js
@@ -3,8 +3,8 @@ function SendMail() {
         from_name: document.getElementById("fullName").value,
         email_id: document.getElementById("email_id").value,
         number : document.getElementById("number").value,
-        email_id: document.getElementById("richiamare").value,
-        msg: document.getElementById("message").value
+        richiamare: document.getElementById("richiamare").value,
+        msg: document.getElementById("msg").value
     }
     emailjs.send("service_9f61txk", "template_sx8rlqg", params).then(function (res) {
         alert("Email inviata correttamente");
