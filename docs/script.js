c23d75b35dd2544a35d3f90468fb9597d2d5f0ea
brunosvara <brunosvara032@gmail.com>
2023-08-23 18:04:37 +0200

    aggiornamento

---
+++ docs/script.js
@@ -0,0 +1,18 @@
+document.getElementById("myForm").addEventListener("submit", function (event) {
+    event.preventDefault(); // Previeni il comportamento predefinito del browser
+    SendMail();
+});
+
+function SendMail() {
+    var params = {
+        fullName : document.getElementById("fullName").value,
+        email_id: document.getElementById("email_id").value,
+        number: document.getElementById("number").value,
+        richiamare: document.getElementById("richiamare").value,
+        msg: document.getElementById("msg").value
+    };
+
+    emailjs.send("service_9f61txk", "template_sx8rlqg", params).then(function (res) {
+        alert("Email inviata correttamente");
+    });
+}
