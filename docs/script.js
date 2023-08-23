230ce625ca09ff53ba7c92bedca645edd673f2a6
brunosvara <brunosvara032@gmail.com>
2023-08-23 16:56:17 +0200

    aggiornamento contatti

--- docs/script.js
+++ docs/script.js
@@ -2,9 +2,11 @@ function SendMail() {
     var params = {
         from_name: document.getElementById("fullName").value,
         email_id: document.getElementById("email_id").value,
-        message: document.getElementById("message").value
+        number : document.getElementById("number").value,
+        email_id: document.getElementById("richiamare").value,
+        msg: document.getElementById("message").value
     }
     emailjs.send("service_9f61txk", "template_sx8rlqg", params).then(function (res) {
-        alert("Success! " + res.status);
+        alert("Email inviata correttamente");
     })
 }
