0ed17365c142d5a30580b39b174dcb44aa8e0013
brunosvara <brunosvara032@gmail.com>
2023-08-26 23:11:28 +0200

    definitivo

--- docs/script.js
+++ docs/script.js
@@ -1,18 +1,46 @@
-document.getElementById("myForm").addEventListener("submit", function (event) {
-    event.preventDefault(); // Previeni il comportamento predefinito del browser
-    SendMail();
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("myForm").addEventListener("submit", function (event) {
+        event.preventDefault();
+        SendMail();
+    });
+
+    // Ottenere tutti gli elementi h3 delle FAQ solo se non sono già stati ottenuti
+    const faqTitles = document.querySelectorAll('.contact .row .faq .box h3');
+    if (faqTitles) {
+        // Aggiungere l'evento di clic a ciascun titolo delle FAQ
+        faqTitles.forEach(faqTitle => {
+            faqTitle.addEventListener('click', () => {
+                // Ottenere il genitore dell'elemento h3 (la casella delle FAQ)
+                const faqBox = faqTitle.parentElement;
+
+                // Aggiungere o rimuovere la classe 'active' per mostrare o nascondere il contenuto
+                faqBox.classList.toggle('active');
+            });
+        });
+    }
 });
 
 function SendMail() {
     var params = {
-        fullName : document.getElementById("fullName").value,
+        from_name: document.getElementById("fullName").value,
         email_id: document.getElementById("email_id").value,
         number: document.getElementById("number").value,
         richiamare: document.getElementById("richiamare").value,
         msg: document.getElementById("msg").value
     };
 
-    emailjs.send("service_9f61txk", "template_sx8rlqg", params).then(function (res) {
-        alert("Email inviata correttamente");
-    });
+    emailjs.send("service_9f61txk", "template_sx8rlqg", params).then(
+        function (res) {
+            alert("Email inviata correttamente");
+            document.getElementById("fullName").value = '';
+            document.getElementById("email_id").value = '';
+            document.getElementById("number").value = '';
+            document.getElementById("richiamare").value = '';
+            document.getElementById("msg").value = '';
+        },
+        function (error) {
+            console.log("Errore nell'invio dell'email:", error);
+            alert("Si è verificato un errore nell'invio dell'email. Controlla la console per i dettagli.");
+        }
+    );
 }
